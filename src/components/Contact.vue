f9a8d866b689c9ef4e8c50f5a98ffa84cfd8709a
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-04-17 18:19:49 -0700

    started grid project layout

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,7 +7,7 @@
             <h5>Email:</h5>
             <p>Elianrenteriadevelopment@gmail.com</p>
             <h5>Phone:</h5>
-            <p>+1234567890</p>
+            <p>+1 909-314-3799</p>
             <h5>Social Media:</h5>
             <div class="social-links">
                 <a href="https://linkedin.com/in/elian-renteria-5a467b29b" target="_blank"><i class="pi pi-linkedin" style="font-size: 2.5rem"></i></a>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from 'axios';
-//const githubToken = require('./github_repo_key.json');
+import Card from 'primevue/card';
 import Loading from './LoadingProjects.vue';
 
 export default {
@@ -13,40 +13,34 @@ export default {
   },
   components: {
     Loading,
+    Card
   },
   mounted() {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 4000); 
-    setTimeout(() => {
-      this.fadeInContent();
-    }, 4300);
     // Fetch GitHub projects
     this.fetchProjects();
   },
   methods: {
-    fadeInContent() {
-      const content = document.querySelector('.content');
-      content.classList.add('fade-in');
-    },
     async fetchProjects() {
       try {
         let allRepos = [];
         let page = 1;
         let response;
-        
+
+        // Fetch repositories
         do {
-          response = await axios.get(`https://api.github.com/users/ElianRenteria/repos?page=${page}&per_page=100`, {
-            headers: {
-              Authorization: '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'//`Bearer ${githubToken.token}`
-            }
-          });
-          
+          response = await axios.get(`https://api.github.com/users/ElianRenteria/repos?page=${page}&per_page=100`);
           allRepos = [...allRepos, ...response.data];
           page++;
         } while (response.data.length === 100);
-  
+
+        // Fetch languages for each repository
+        await Promise.all(allRepos.map(async repo => {
+          const languagesResponse = await axios.get(repo.languages_url);
+          repo.languages = Object.keys(languagesResponse.data);
+        }));
+
         this.projects = allRepos;
+        this.isLoading = false;
       } catch (error) {
         console.error('Error fetching GitHub projects:', error);
       }
@@ -55,15 +49,23 @@ export default {
 };
 </script>
 
-
-
 <template>
-  <div>
+  <div class="projects-grid">
     <h2>My Projects</h2>
-    <ul>
+    <Loading v-if="isLoading" />
+    <ul class="grid-container">
       <li v-for="project in projects" :key="project.id">
-        <a :href="project.html_url" target="_blank">{{ project.name }}</a>
-        <p>{{ project.description }}</p>
+        <Card>
+          <template #title>
+            <a class="p-card-title" :href="project.html_url" target="_blank">{{ project.name }}</a>
+          </template>
+          <template #subtitle>
+            <p>{{ project.languages.join(', ') }}</p>
+          </template>
+          <template #content>
+            <p>{{ project.description }}</p>
+          </template>
+        </Card>
       </li>
     </ul>
   </div>
@@ -71,23 +73,49 @@ export default {
 
 <style scoped>
 ul {
-  list-style-type: none; 
-  padding: 0; 
+  list-style-type: none;
+  padding: 0;
 }
 
 li {
-  margin-bottom: 10px; 
+  margin-bottom: 10px;
 }
 
 a {
-  text-decoration: none; 
+  text-decoration: none;
   color: #ffffff;
+  white-space: nowrap; /* Prevent title from wrapping */
+  overflow: hidden; /* Hide overflow */
+  text-overflow: ellipsis; /* Display ellipsis for overflow */
 }
+
 a:hover {
   color: #fbbf24;
 }
 
 p {
-  margin-top: 5px; 
+  margin-top: 5px;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); /* Adjust the column width as needed */
+  grid-gap: 20px; /* Adjust the gap between grid items */
+  list-style: none;
+  padding: 0;
+}
+
+/* Adjust title font size dynamically */
+.p-card-title {
+  font-size: calc(12px + (14 - 12) * ((100vw - 300px) / (1600 - 300))); /* Adjust the min and max font size as needed */
+}
+.projects-grid {
+  margin: 0 auto;
+  padding: 0%;
+  width: 100%;
+  height: 100%;
+}
+.p-card{
+  overflow: hidden !important;
 }
 </style>
