48b39808d777a169457ce1b8f3c4fd2daedaf05c
ElianRenteria <elianrenteriadevelopment@gmail.com>
2024-04-24 11:19:15 -0700

    fixed project load spinner

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import 'primeicons/primeicons.css';
 import Ripple from 'primevue/ripple';
 import router from './router/index';
 import Menu from 'primevue/menu';
+import ProgressSpinner from 'primevue/progressspinner';
 
 const app = createApp(App);
 
 app.use(router); 
 app.use(PrimeVue, { ripple: true });
 app.component('Menu', Menu);
+app.component('ProgressSpinner', ProgressSpinner);
 app.directive('ripple', Ripple);
 app.mount('#app');
