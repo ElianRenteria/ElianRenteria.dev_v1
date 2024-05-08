f9f3f0bf0da21813fa3d61e925c95895a6dee79c
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-05-07 18:01:53 -0700

    removed proess.env

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new Dotenv()
-    ]
-  }
+  transpileDependencies: true
 });
